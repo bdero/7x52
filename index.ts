6c7461465e4fa6b09a93c06b5968310ef964bd0c
Brandon DeRosier <x@bdero.me>
2018-07-17 01:02:40 -0700

    Set canvas size

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
-const loaded = () => {
-    const stub = document.getElementById("main")
-    stub.textContent = "hey"
+const SQUARE_SIZE = 10 // Pixels
+const GRID_WIDTH = 52
+const GRID_HEIGHT = 7
+
+let canvas : HTMLCanvasElement
+
+const init : EventListener = () : void => {
+    canvas = <HTMLCanvasElement> document.getElementById("canvas")
+    canvas.width = SQUARE_SIZE*GRID_WIDTH
+    canvas.height = SQUARE_SIZE*GRID_HEIGHT
 }
-window.addEventListener('DOMContentLoaded', loaded)
+window.addEventListener('DOMContentLoaded', init)
