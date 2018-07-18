59c6b4e388dfea4872cfeb0589567c535ec624d6
Brandon DeRosier <x@bdero.me>
2018-07-18 00:55:56 -0700

    Random colors

--- src/color.ts
+++ src/color.ts
@@ -13,19 +13,26 @@ class Color {
         this.a = a
     }
 
-    public toString() {
+    public toString() : string {
         if (this.a == undefined) {
             return `rgb(${this.r},${this.g},${this.b})`
         }
         return `rgba(${this.r},${this.g},${this.b},${this.a})`
     }
 
-    public static lerp(a : Color, b : Color, alpha : number) {
+    public static lerp(a : Color, b : Color, alpha : number) : Color {
         return new Color(
             lerp(a.r, b.r, alpha),
             lerp(a.g, b.g, alpha),
             lerp(a.b, b.b, alpha),
             a.a != undefined && b.a != undefined ? lerp(a.a, b.a, alpha) : undefined,
+        )
+    }
+
+    public static random(alpha : boolean = false) : Color {
+        return new Color(
+            Math.random()*255, Math.random()*255, Math.random()*255,
+            alpha ? Math.random()*255 : undefined
         )
     }
 }
