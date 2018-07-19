ab5826950ed719872e9980709ef9958702fab6ef
Brandon DeRosier <x@bdero.me>
2018-07-18 21:45:08 -0700

    Add momentjs and format string

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
+import moment from 'moment-es6'
 import Color from './color'
 
 const SQUARE_SIZE = 20 // Pixels
@@ -8,8 +9,14 @@ const GRID_UNIT_MARGIN = 2
 const COLOR_OFF = new Color(100, 100, 100)
 const COLOR_ON = new Color(200, 200, 200)
 
+const today = moment()
+const FIRST_DAY : moment.Moment = today.subtract(today.weekday() + 52*7)
+
+// Example ouput: Wed Jul 18 20:54:28 2018 -0700
+const GIT_DATE_FORMAT = 'ddd MMM d HH:mm:ss Y ZZ'
+
 export {
-    SQUARE_SIZE, GRID_WIDTH, GRID_HEIGHT, GRID_UNIT_MARGIN
-    COLOR_OFF,
-    COLOR_ON
+    SQUARE_SIZE, GRID_WIDTH, GRID_HEIGHT, GRID_UNIT_MARGIN,
+    COLOR_OFF, COLOR_ON,
+    FIRST_DAY, GIT_DATE_FORMAT,
 }
